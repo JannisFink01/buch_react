6122f3506c18f5c24b8e7d334b866d81f5293e6a
JannisFink01 <fija1016@h-ka.de>
2023-06-06 21:20:47 +0200

    such from and deleted navbar

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,38 @@ export function App() {
     })
   }
   return <div className='Buch'>
-    <Navbar/>
+    <h1>Bücher anlegen</h1>
+      <form>
+  <div className="form-group">
+    <label htmlFor="exampleInputisbn1">ISBN</label>
+    <input type="isbn" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ISBN"/>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputTitel1">Titel</label>
+    <input type="Titel" className="form-control" id="exampleInputTitel1" placeholder="Titel"/>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputUnterttitel1">Untertitel</label>
+    <input type="Untertitel" className="form-control" id="exampleInputuntertitel1" aria-describedby="untertitelHelp" placeholder="Enter untertitel"/>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputRating1">Rating</label>
+    <input type="Rating" className="form-control" id="exampleInputRating1" aria-describedby="RatingHelp" placeholder="Enter Rating"/>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputArt1">Art</label>
+    <input type="Art" className="form-control" id="exampleInputArt1" aria-describedby="RatingHelp" placeholder="Enter Art"/>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputPreis1">Art</label>
+    <input type="Art" className="form-control" id="exampleInputArt1" aria-describedby="RatingHelp" placeholder="Enter Art"/>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
     <header className='buch-header'>
       <Button onClick={getBook}>Get Book</Button>
       {showTable&&<BuchTable buecher ={buecher}/>}
      </header>
+     
  </div>	
 }
