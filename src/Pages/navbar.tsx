c8574d756127a3521d3adcf02fd2c480f4d50822
JannisFink01 <fija1016@h-ka.de>
2023-06-14 15:12:31 +0200

    navbar verschönerung

--- src/Pages/navbar.tsx
+++ src/Pages/navbar.tsx
@@ -4,31 +4,31 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <ul className="nav navbar-nav navbar-right">
-                    <li className="active">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <NavDropdown title="Search" id="basic-nav-dropdown">
-                        <Link className="dropdown-item" to="/search/isbn">
-                            Search ISBN
-                        </Link>
-                        <Link className="dropdown-item" to="/search/title">
-                            Search Title
-                        </Link>
-                        <Link className="dropdown-item" to="search/all">
-                            Search All
-                        </Link>
-                    </NavDropdown>
-                    <li>
-                        <Link to="/create">Create</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div className="container-fluid">
+          <ul className="nav navbar-nav navbar-right">
+            <li className="active">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li>
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
+            <NavDropdown title="Search" id="basic-nav-dropdown">
+              <Link className="dropdown-item" to="/search/isbn">
+                Search ISBN
+              </Link>
+              <Link className="dropdown-item" to="/search/title">
+                Search Title
+              </Link>
+              <Link className="dropdown-item" to="/search/all">
+                Search All
+              </Link>
+            </NavDropdown>
+            <li>
+              <Link to="/create" className="nav-link">Create</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
 }
 
