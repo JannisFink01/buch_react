779419c7cffa300ea73f239b915d4b949b260c2e
bichanhoang <bichanhoang00@gmail.com>
2023-06-14 15:11:32 +0200

    fixed navbar to appear on all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Pages/navbar';
 
 export function App() {
     return (
         <div className="Buch">
             <h1>Welcome to BIGBOOKS!</h1>
             <div className="bücher-menu">
-                <Navbar />
             </div>
         </div>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
+import Navbar from './Pages/navbar.tsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
@@ -9,31 +9,62 @@ import Get from './Pages/Get/get.tsx';
 import { Login } from './Pages/auth/login.tsx';
 import FindByISBN from './Pages/Get/findByISBN.tsx';
 import FindByTitle from './Pages/Get/findByTitel.tsx';
+import { App } from './App.tsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />,
+        element: (
+            <>
+                <App />
+                <Navbar />
+            </>
+        ),
     },
     {
         path: '/create',
-        element: <Formular />,
+        element: (
+            <>
+                <Navbar />
+                <Formular />
+            </>
+        ),
     },
     {
         path: '/login',
-        element: <Login />,
+        element: (
+            <>
+                <Navbar />
+                <Login />
+            </>
+        ),
     },
     {
         path: 'search/isbn',
-        element: <FindByISBN />,
+        element: (
+            <>
+                <Navbar />
+                <FindByISBN />
+            </>
+        ),
     },
     {
         path: 'search/title',
-        element: <FindByTitle />,
+        element: (
+            <>
+                <Navbar />
+                <FindByTitle />
+            </>
+        ),
     },
     {
         path: 'search/all',
-        element: <Get />,
+        element: (
+            <>
+                <Navbar />
+                <Get />
+            </>
+        ),
     },
 ]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
