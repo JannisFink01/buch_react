6513ad5c1ff2ec3b056133586321e1cb09af514e
JannisFink01 <fija1016@h-ka.de>
2023-06-16 17:04:10 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export function App() {
     return (
         <div className="Buch">
-        <Card>
-          <Card.Img variant="top" src="images/pexels-gül-işık-2128249.jpg" />
-          <Card.Body>
-            <h1>Wilkommen bei Bigbooks!</h1>
-            <Button variant='primary'className='w-100 mb-3'>
-            <Link to="/create" className="nav-link">
-                <FontAwesomeIcon icon={faPlus} /> Buch anlegen
-              </Link>
-            </Button>
-            <Button variant="primary" className="w-100 mb-3">
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" className='custom-dropdown-toggle'>
-                <FontAwesomeIcon icon={faSearch} /> Bücher suchen
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} to="/search/all">
-                  Alle Bücher
-                </Dropdown.Item>
-                <Dropdown.Item as={Link} to="/search/isbn">
-                  Suche nach ISBN
-                </Dropdown.Item>
-                <Dropdown.Item as={Link} to="/search/title">
-                  Suche nach Titel
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-           </Button>
-          </Card.Body>
-        </Card>
-        <div className="bücher-menu"></div>
-      </div>
+            <Card>
+                <Card.Img
+                    variant="top"
+                    src="images/pexels-gül-işık-2128249.jpg"
+                />
+                <Card.Body>
+                    <h1>Wilkommen bei Bigbooks!</h1>
+                    <Button variant="primary" className="w-100 mb-3">
+                        <Link to="/create" className="nav-link">
+                            <FontAwesomeIcon icon={faPlus} /> Buch anlegen
+                        </Link>
+                    </Button>
+                    <Button variant="primary" className="w-100 mb-3">
+                        <Dropdown>
+                            <Dropdown.Toggle
+                                variant="primary"
+                                className="custom-dropdown-toggle"
+                            >
+                                <FontAwesomeIcon icon={faSearch} /> Bücher
+                                suchen
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item as={Link} to="/search/all">
+                                    Alle Bücher
+                                </Dropdown.Item>
+                                <Dropdown.Item as={Link} to="/search/isbn">
+                                    Suche nach ISBN
+                                </Dropdown.Item>
+                                <Dropdown.Item as={Link} to="/search/title">
+                                    Suche nach Titel
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Button>
+                </Card.Body>
+            </Card>
+            <div className="bücher-menu"></div>
+        </div>
     );
 }
--- src/Pages/navbar.tsx
+++ src/Pages/navbar.tsx
@@ -2,85 +2,109 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faSearch,
-  faSignInAlt,
-  faPlus,
-  faBook,
+    faSearch,
+    faSignInAlt,
+    faPlus,
+    faBook,
 } from '@fortawesome/free-solid-svg-icons';
 import { NavDropdown } from 'react-bootstrap';
 import './navbar.css';
 import { Logout } from './auth/logout';
 
 function Navbar() {
-  const [isExpanded, setIsExpanded] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleNavbar = () => {
-    setIsExpanded(!isExpanded);
-  };
-  const handleLinkClick = () => {
-    setIsExpanded(false); // Schließt die Toggle-Navigation
-  };
+    const toggleNavbar = () => {
+        setIsExpanded(!isExpanded);
+    };
+    const handleLinkClick = () => {
+        setIsExpanded(false); // Schließt die Toggle-Navigation
+    };
 
-  return (
-    <nav
-      className="navbar navbar-expand-lg navbar-light fixed-top"
-      style={{ backgroundColor: '#e3f2fd' }}
-    >
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
-          <FontAwesomeIcon icon={faBook} /> BIGBOOKS
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={toggleNavbar}
+    return (
+        <nav
+            className="navbar navbar-expand-lg navbar-light fixed-top"
+            style={{ backgroundColor: '#e3f2fd' }}
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className={`collapse navbar-collapse justify-content-end ${
-            isExpanded ? 'show' : ''
-          }`}
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavDropdown
-                title={
-                  <span>
-                    <FontAwesomeIcon icon={faSearch} /> Bücher suchen
-                  </span>
-                }
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item as={Link} to="/search/all" onClick={handleLinkClick}>
-                  Alle Bücher
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/search/isbn" onClick={handleLinkClick}>
-                  Suche nach ISBN
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/search/title" onClick={handleLinkClick}>
-                  Suche nach Titel
-                </NavDropdown.Item>
-              </NavDropdown>
-            </li>
-            <li className="nav-item">
-              <Link to="/create" className="nav-link" onClick={handleLinkClick}>
-                <FontAwesomeIcon icon={faPlus} /> Buch anlegen
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link" onClick={handleLinkClick}>
-                <FontAwesomeIcon icon={faSignInAlt} /> Login
-              </Link>
-            </li>
-            <li className="nav-item text-center" onClick={handleLinkClick}>
-              <Logout/>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">
+                    <FontAwesomeIcon icon={faBook} /> BIGBOOKS
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={toggleNavbar}
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className={`collapse navbar-collapse justify-content-end ${
+                        isExpanded ? 'show' : ''
+                    }`}
+                >
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavDropdown
+                                title={
+                                    <span>
+                                        <FontAwesomeIcon icon={faSearch} />{' '}
+                                        Bücher suchen
+                                    </span>
+                                }
+                                id="basic-nav-dropdown"
+                            >
+                                <NavDropdown.Item
+                                    as={Link}
+                                    to="/search/all"
+                                    onClick={handleLinkClick}
+                                >
+                                    Alle Bücher
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    as={Link}
+                                    to="/search/isbn"
+                                    onClick={handleLinkClick}
+                                >
+                                    Suche nach ISBN
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    as={Link}
+                                    to="/search/title"
+                                    onClick={handleLinkClick}
+                                >
+                                    Suche nach Titel
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/create"
+                                className="nav-link"
+                                onClick={handleLinkClick}
+                            >
+                                <FontAwesomeIcon icon={faPlus} /> Buch anlegen
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/login"
+                                className="nav-link"
+                                onClick={handleLinkClick}
+                            >
+                                <FontAwesomeIcon icon={faSignInAlt} /> Login
+                            </Link>
+                        </li>
+                        <li
+                            className="nav-item text-center"
+                            onClick={handleLinkClick}
+                        >
+                            <Logout />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
